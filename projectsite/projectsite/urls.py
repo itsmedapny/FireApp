d66fbad148c500c39f6f72fb060b077ee3ed1764
itsmedapny <madaphneeevediente@gmail.com>
2024-04-27 01:57:25 +0800

    Successful chart setup

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path
 
 from fire.views import HomePageView
-from fire import views
+from fire import views 
+from fire.views import HomePageView, ChartView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
+    path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
 
 ]
